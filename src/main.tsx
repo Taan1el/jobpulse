7181c7797deab28bdd93777c7dfbea0df495fe9a
Taan1el <145098766+Taan1el@users.noreply.github.com>
2026-09-18 06:25:31 +0300

    Switch to the shared design tokens and self-hosted fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/sora'
+import '@fontsource-variable/geist'
+import '@fontsource-variable/geist-mono'
+import './styles/tokens.css'
 import './index.css'
 import App from './App.tsx'
 
